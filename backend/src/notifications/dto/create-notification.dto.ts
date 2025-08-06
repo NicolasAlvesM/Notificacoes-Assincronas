6f912f9bded6b9d68f52c2edd64cb9f1cf8db9d5
NicolasAlvesM <nicolas.morenoam@outlook.com>
2025-08-05 23:02:40 -0300

    fix: notification dto

--- backend/src/notifications/dto/create-notification.dto.ts
+++ backend/src/notifications/dto/create-notification.dto.ts
@@ -1,6 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateNotificationDto {
+  @IsUUID()
+  @IsNotEmpty({ message: 'O id não pode ser vazio.' })
+  id: string;
+
   @IsString()
   @IsNotEmpty({ message: 'O conteudoMensagem não pode ser vazio.' })
   conteudoMensagem: string;
